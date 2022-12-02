15eca127eda0a649f680180ab99462be607cced5
THA <thanhtaikaung90@gmail.com>
2022-12-02 23:24:46 +0630

    build and send a simple notification

--- rider/src/main/java/com/tha/rider/MainActivity.kt
+++ rider/src/main/java/com/tha/rider/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.tha.rider
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.tha.shared.showSnackBar
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +9,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
         showSnackBar("This is rider app")
+
+        sendNotification(
+            this,
+            title = "Hello",
+            body = "Notifications provide short, timely information about events in your app while it's not in use. This page teaches you how to create a notification with various features for Android 4.0 (API level 14) and higher. For an introduction to how notifications appear on Android, see the Notifications Overview. "
+        )
     }
 }
